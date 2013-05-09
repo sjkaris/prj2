2998936b9ef3050d4b70466b561a5f4e2432556d
Sumeet-Jain <sumeetjain@berkeley.edu>
2013-05-08 19:49:19 -0700

    here stevie

---
+++ Network-Project/dict/HashTableChained.java
@@ -0,0 +1,239 @@
+/* HashTableChained.java */
+
+package dict;
+import list.*;
+
+/**
+ *  HashTableChained implements a Dictionary as a hash table with chaining.
+ *  All objects used as keys must have a valid hashCode() method, which is
+ *  used to determine which bucket of the hash table an entry is stored in.
+ *  Each object's hashCode() is presumed to return an int between
+ *  Integer.MIN_VALUE and Integer.MAX_VALUE.  The HashTableChained class
+ *  implements only the compression function, which maps the hash code to
+ *  a bucket in the table's range.
+ *
+ *  DO NOT CHANGE ANY PROTOTYPES IN THIS FILE.
+ **/
+
+public class HashTableChained implements Dictionary {
+
+  /**
+   *  Place any data fields here.
+   **/
+
+    int size;
+    List[] arr;
+
+  /** 
+   *  Construct a new empty hash table intended to hold roughly sizeEstimate
+   *  entries.  (The precise number of buckets is up to you, but we recommend
+   *  you use a prime number, and shoot for a load factor between 0.5 and 1.)
+   **/
+
+    //Load factor of 1
+  public HashTableChained(int sizeEstimate) {
+     size = 0;
+      int sizeTable = sizeEstimate;
+      while(!prime(sizeTable)){
+        sizeTable++;
+      }
+      if(sizeTable < 0){
+        sizeTable = 2000000011;
+      }
+      arr = new SList[sizeTable];
+      for(int i = 0; i < arr.length; i++)
+          arr[i] = new SList();
+
+  }
+
+  public boolean prime(int p){
+    for(int i = 2; i < Math.sqrt(p); i++){
+      if(p%i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /** 
+   *  Construct a new empty hash table with a default size.  Say, a prime in
+   *  the neighborhood of 100.
+   **/
+
+  public HashTableChained() {
+      size = 0;
+      arr = new SList[101];
+      for(int i = 0; i < arr.length; i++)
+          arr[i] = new SList();
+  }
+
+  /**
+   *  Converts a hash code in the range Integer.MIN_VALUE...Integer.MAX_VALUE
+   *  to a value in the range 0...(size of hash table) - 1.
+   *
+   *  This function should have package protection (so we can test it), and
+   *  should be used by insert, find, and remove.
+   **/
+
+  int compFunction(int code) {
+    // Replace the following line with your solution.
+    return Math.abs(code) % arr.length;
+  }
+
+  /** 
+   *  Returns the number of entries stored in the dictionary.  Entries with
+   *  the same key (or even the same key and value) each still count as
+   *  a separate entry.
+   *  @return number of entries in the dictionary.
+   **/
+
+  public int size() {
+    return size;
+  }
+
+  /** 
+   *  Tests if the dictionary is empty.
+   *
+   *  @return true if the dictionary has no entries; false otherwise.
+   **/
+
+  public boolean isEmpty() {
+    // Replace the following line with your solution.
+    return size == 0;
+  }
+
+  /**
+   *  Create a new Entry object referencing the input key and associated value,
+   *  and insert the entry into the dictionary.  Return a reference to the new
+   *  entry.  Multiple entries with the same key (or even the same key and
+   *  value) can coexist in the dictionary.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the key by which the entry can be retrieved.
+   *  @param value an arbitrary object.
+   *  @return an entry containing the key and value.
+   **/
+
+  public Entry insert(Object key, Object value) {
+      int pos = compFunction(key.hashCode());
+      List theList = arr[pos];
+      Entry holder = new Entry();
+      holder.key = key;
+      holder.value = value;
+      theList.insertFront(holder);
+      size++;
+      return holder;
+  }
+
+  /** 
+   *  Search for an entry with the specified key.  If such an entry is found,
+   *  return it; otherwise return null.  If several entries have the specified
+   *  key, choose one arbitrarily and return it.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the search key.
+   *  @return an entry containing the key and an associated value, or null if
+   *          no entry contains the specified key.
+   **/
+
+  public Entry find(Object key) {
+    // Replace the following line with your solution.
+    try{
+        int pos = compFunction(key.hashCode());
+        List theList = arr[pos];
+        ListNode node = theList.front();
+        Entry currEntry = (Entry) node.item();
+        while(!currEntry.key.equals(key)){
+            node = node.next();
+            currEntry = (Entry) node.item();
+        }
+        return currEntry;
+    } catch(InvalidNodeException x) {
+        return null;
+    }
+
+  }
+
+  /** 
+   *  Remove an entry with the specified key.  If such an entry is found,
+   *  remove it from the table and return it; otherwise return null.
+   *  If several entries have the specified key, choose one arbitrarily, then
+   *  remove and return it.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the search key.
+   *  @return an entry containing the key and an associated value, or null if
+   *          no entry contains the specified key.
+   */
+
+  public Entry remove(Object key) {
+    // Replace the following line with your solution.
+    try{
+        int pos = compFunction(key.hashCode());
+        List theList = arr[pos];
+        ListNode node = theList.front();
+        Entry currEntry = (Entry) node.item();
+        while(!currEntry.key.equals(key)){
+            node = node.next();
+            currEntry = (Entry) node.item();
+        }
+        node.remove();
+        size--;
+        return currEntry;
+    } catch(InvalidNodeException x) {
+        return null;
+    }
+  }
+
+  /**
+   *  Remove all entries from the dictionary.
+   */
+  public void makeEmpty() {
+      arr = new SList[arr.length];
+      size = 0;
+      for(int i = 0; i < arr.length; i++)
+          arr[i] = new SList();
+
+  }
+
+  int times = 0;
+
+  public void test(){
+      int count = 0;
+      for(int i = 0; i < arr.length ; i++){
+          System.out.print("Hash " + i + " : ");
+          boolean subtract = false;
+          for(int j = 0; j < arr[i].length(); j++){
+              System.out.print("X");
+              count++;
+              subtract = true;
+          }
+          if(subtract)
+              count--;
+          System.out.println();
+      }
+      System.out.println("-----------------------------");
+      System.out.println("I have: " + count + " collisions");
+  }
+
+    public static boolean[] getPrimes(int max){
+        boolean[] isPrime = new boolean[max + 1];
+        int i = 2;
+        for(int k = 0; k < isPrime.length; k++)
+            isPrime[k] = true;
+        isPrime[0]  = false;
+        isPrime[1] = false;
+        while(i < isPrime.length){
+            if(isPrime[i]){
+                for(int j = i+i; j < isPrime.length; j += i)
+                    isPrime[j] = false;
+            }
+            i++;
+        }
+        return isPrime;
+    }
+
+}
---
+++ Network-Project/tests/testBoard.java
@@ -0,0 +1,312 @@
+package tests;
+
+import player.*;
+import tree.*;
+
+public class testBoard{
+	
+	public static void p(Object o){
+        System.out.println(o);
+    }
+
+    public static void main(String[] args){
+        Board b = new Board();
+        System.out.println("Printing Empty Board");
+        System.out.println(b);
+
+        System.out.println("Testing valid moves on an empty board. First with black, then white");
+        System.out.println("The following should be 48: " + b.validMoves(Square.WHITE).length());
+        System.out.println("The following should be 48: " + b.validMoves(Square.BLACK).length());
+
+        //Other cases
+        Board test=new Board();
+		 System.out.println("add 5 black pieces, such that only one more piece at (5,0) will make a network");
+		 Move addBlack1=new Move(3,3);
+		 test.updateBoard(addBlack1,Square.BLACK);
+		 Move addBlack2=new Move(5,3);
+		 test.updateBoard(addBlack2,Square.BLACK);
+		 Move addBlack3=new Move(2,5);
+		 test.updateBoard(addBlack3,Square.BLACK);
+		 Move addBlack4=new Move(5,5);
+		 test.updateBoard(addBlack4,Square.BLACK);
+		 Move addBlack5=new Move(2,7);
+		 test.updateBoard(addBlack5,Square.BLACK);
+
+         System.out.println(test);
+		 
+		 System.out.println("add 6 stupidly placed white pieces");
+		 Move addWhite1=new Move(0,2);
+		 test.updateBoard(addWhite1,Square.WHITE);
+		 Move addWhite2=new Move(2,1);
+		 test.updateBoard(addWhite2,Square.WHITE);
+		 Move addWhite3=new Move(2,4);
+		 test.updateBoard(addWhite3,Square.WHITE);
+		 Move addWhite4=new Move(3,6);
+		 test.updateBoard(addWhite4,Square.WHITE);
+		 Move addWhite5=new Move(6,2);
+		 test.updateBoard(addWhite5,Square.WHITE);
+		 Move addWhite6=new Move(6,6);
+		 test.updateBoard(addWhite6,Square.WHITE);
+
+         System.out.println(test);
+
+		
+		 //TESTS FOR getColor//
+         p("Tests for color");
+		 //Should return 1
+		 System.out.println("Should return 1: " + test.getColor(3, 3));
+		//Should return 0
+		 System.out.println("Should return 0: " +test.getColor(2,1));
+		//Should return -1
+		 System.out.println("Should return -1: " +test.getColor(0,0));
+		//Should return -1
+		 System.out.println("Should return -1: " +test.getColor(-3,0));
+		//Should return -1
+		 System.out.println("Should return -1: " +test.getColor(4,4));
+		 
+		//TESTS FOR checkMove//
+		 p("");
+		 p("******************************");
+		 p("Tests for checkMove");
+
+		 Move whereTo=new Move(5,0);
+		 System.out.println("Should return true, since it's in the goal: "+test.checkMove(whereTo, Square.BLACK));
+		 Move whereTo2=new Move(7,1);
+		 System.out.println("Should return false, since it's in another goal: "+test.checkMove(whereTo2, Square.BLACK));
+		 Move whereTo3=new Move(6,6);
+		 System.out.println("Should return false, since there's already a piece there: "+test.checkMove(whereTo3, Square.BLACK));
+		 Move whereTo4=new Move(4,3);
+		 System.out.println("Should return false, since there's two adjacent pieces there: "+test.checkMove(whereTo4, Square.BLACK));
+		 Move whereTo5=new Move(8,6);
+		 System.out.println("Should return false, since the piece isn't on the board: "+test.checkMove(whereTo5, Square.BLACK));
+		 
+		 //More checkMove tests
+		 //Add new black pieces
+		 Board testGoals = new Board();
+		 Move goalBlack1=new Move(3,0);
+		 testGoals.updateBoard(goalBlack1,Square.BLACK);
+		 Move goalBlack2=new Move(4,0);
+		 testGoals.updateBoard(goalBlack2,Square.BLACK);
+		 Move goalBlack3=new Move(1,7);
+		 testGoals.updateBoard(goalBlack3,Square.BLACK);
+		 Move goalBlack4=new Move(6,7);
+		 testGoals.updateBoard(goalBlack4,Square.BLACK);
+		 //Add new white pieces
+		 Move goalWhite1=new Move(0,3);
+		 testGoals.updateBoard(goalWhite1,Square.WHITE);
+		 Move goalWhite2=new Move(0,5);
+		 testGoals.updateBoard(goalWhite2,Square.WHITE);
+		 Move goalWhite3=new Move(7,2);
+		 testGoals.updateBoard(goalWhite3,Square.WHITE);
+		 Move goalWhite4=new Move(7,4);
+		 testGoals.updateBoard(goalWhite4,Square.WHITE);
+		 
+		 p("");
+		 p("******************************");
+		 p("More Tests for checkMove");
+		 testGoals = new Board();
+		 p(testGoals);
+		 p("Trying to do a step move without all pieces used, should return false: " + testGoals.checkMove(new Move(1,1,3,3), Square.BLACK));
+		 testGoals.updateBoard(new Move(3,2), Square.BLACK);
+		 testGoals.updateBoard(new Move(3,3), Square.BLACK);
+		 testGoals.updateBoard(new Move(5,6), Square.BLACK);
+		 testGoals.updateBoard(new Move(6,5), Square.BLACK);
+		 testGoals.updateBoard(new Move(1,1), Square.BLACK);
+		 testGoals.updateBoard(new Move(1,4), Square.BLACK);
+		 testGoals.updateBoard(new Move(1,7), Square.BLACK);
+		 testGoals.updateBoard(new Move(6,4), Square.BLACK);
+		 testGoals.updateBoard(new Move(6,0), Square.BLACK);
+		 testGoals.updateBoard(new Move(3,7), Square.BLACK);
+		 p(testGoals);
+		 p("Trying illegal stepMove (moving to too many adj): " + testGoals.checkMove(new Move(3,4,1,1), Square.BLACK));
+		 p("Trying illegal stepMove (no piece in orig loc): " + testGoals.checkMove(new Move(1,2,3,5), Square.BLACK));
+		 p("Trying legal stepMove: " + testGoals.checkMove(new Move(1,2,1,1), Square.BLACK));
+		 p("doing stepMove on above Board: ");
+		 testGoals.updateBoard(new Move(1,2,1,1), Square.BLACK);
+		 p(testGoals);
+		 
+		 p("");
+		 p("******************************");
+		 p("More Tests for checkNetwork");
+		 Board testNetwork = new Board();
+		 testNetwork.updateBoard(new Move(0, 2), Square.WHITE);
+		 testNetwork.updateBoard(new Move(2, 2), Square.WHITE);
+		 testNetwork.updateBoard(new Move(2, 3), Square.WHITE);
+		 testNetwork.updateBoard(new Move(4, 5), Square.WHITE);
+		 testNetwork.updateBoard(new Move(6, 5), Square.WHITE);
+		 testNetwork.updateBoard(new Move(7, 6), Square.WHITE);
+		 p(testNetwork);
+		 p("testing checkNetwork.");
+		 p("Should return true. White has a network: " + testNetwork.checkNetwork(Square.WHITE).hasNetwork());
+		 testNetwork.updateBoard(new Move(6, 5), Square.BLACK);
+		 p(testNetwork);
+		 p("Should return false. White has no network: " + testNetwork.checkNetwork(Square.WHITE).hasNetwork());
+		 testNetwork.updateBoard(new Move(6,3), Square.WHITE);
+		 testNetwork.updateBoard(new Move(7,3), Square.WHITE);
+		 testNetwork.updateBoard(new Move(5,4), Square.BLACK);
+		 p(testNetwork);
+
+		 p("Should return false. White has no network: " + testNetwork.checkNetwork(Square.WHITE).hasNetwork());
+		 testNetwork.updateBoard(new Move(1,1,5,4), Square.BLACK);
+		 p(testNetwork);
+		 p("Should return true. White has a network: " + testNetwork.checkNetwork(Square.WHITE).hasNetwork());
+		 p("Should return 6: " + testNetwork.checkNetwork(Square.WHITE).longestPath());
+
+		 
+		 p("");
+		 p("******************************");
+		 p("More tests for validMoves");
+		 Board testMove = new Board();
+		 p("Valid moves on empty board, should be 48: " + testMove.validMoves(Square.WHITE).length());
+
+
+         p("\n\n\n--------------------------------------------------------");
+         p("Testing new board");
+         Board b2 = new Board();
+         b2.updateBoard(new Move(0,4), Square.WHITE);
+         b2.updateBoard(new Move(4,4), Square.WHITE);
+         b2.updateBoard(new Move(7,4), Square.WHITE);
+         b2.updateBoard(new Move(2,6), Square.WHITE);
+         b2.updateBoard(new Move(4,6), Square.WHITE);
+         b2.updateBoard(new Move(7,6), Square.WHITE);
+         b2.updateBoard(new Move(4,1), Square.WHITE);
+
+         p(b2);
+         ConnectTree n = b2.checkNetwork(Square.WHITE);
+         p("The longest path is: " + n.longestPath());
+         p("Has network should be false. " + n.hasNetwork());
+         p("Num of nodes is: " + n.getNumOfNodes());
+         
+         p("\n\n\n--------------------------------------------------------");
+         p("Testing chooseMove on new Board.");
+         long start = System.currentTimeMillis();
+         MachinePlayer testChoose = new MachinePlayer(0);
+         System.out.println("add 5 black pieces, such that only one more piece at (5,0) will make a network");
+		 addBlack1=new Move(3,3);
+		 testChoose.forceMove(addBlack1);
+		 addBlack2= new Move(5,3);
+		 testChoose.forceMove(addBlack2);
+		 addBlack3=new Move(2,5);
+		 testChoose.forceMove(addBlack3);
+		 addBlack4=new Move(5,5);
+		 testChoose.forceMove(addBlack4);
+		 addBlack5=new Move(3,0);
+		 testChoose.forceMove(addBlack5);
+		 
+		 p(testChoose.getBoard());
+		 p("" + testChoose.chooseMove());
+		 p("Testing to block move: ");
+		 MachinePlayer testBlock = new MachinePlayer(1);
+		 testBlock.opponentMove(addBlack1);
+		 testBlock.opponentMove(addBlack2);
+		 testBlock.opponentMove(addBlack3);
+		 testBlock.opponentMove(addBlack4);
+		 testBlock.opponentMove(addBlack5);
+		 p(testBlock.getBoard());
+		 p("" + testBlock.chooseMove());
+		 Long end = System.currentTimeMillis();
+		 p((start - end)/1000);
+
+		 p("");
+		 p("");
+		 p("-----------------------------------------------");
+		 p("");
+		 MachinePlayer testPlayer1 = new MachinePlayer(1); //white
+		 MachinePlayer testPlayer2 = new MachinePlayer(0, 2); //black
+		 /*while(!testPlayer1.getBoard().checkNetwork(Square.WHITE).hasNetwork() && !testPlayer2.getBoard().checkNetwork(Square.BLACK).hasNetwork()){
+		 	Move temp = testPlayer1.chooseMove();
+		 	p(temp);
+		 	p(testPlayer2.opponentMove(temp));
+		 	p(testPlayer1.getBoard());
+		 	temp = testPlayer2.chooseMove();
+		 	p(temp);
+		 	p(testPlayer1.opponentMove(temp));
+		 	p(testPlayer2.getBoard());
+		 }*/
+
+
+		 testPlayer1.forceMove(new Move(1,1));
+		 testPlayer2.opponentMove(new Move(1,1));
+
+		 testPlayer1.forceMove(new Move(1,2));
+		 testPlayer2.opponentMove(new Move(1,2));
+
+		 testPlayer1.forceMove(new Move(1,4));
+		 testPlayer2.opponentMove(new Move(1,4));
+
+		 testPlayer1.forceMove(new Move(1,5));
+		 testPlayer2.opponentMove(new Move(1,5));
+
+		 testPlayer1.forceMove(new Move(3,1));
+		 testPlayer2.opponentMove(new Move(3,1));
+
+		 testPlayer1.forceMove(new Move(3,2));
+		 testPlayer2.opponentMove(new Move(3,2));
+
+		 testPlayer1.forceMove(new Move(3,4));
+		 testPlayer2.opponentMove(new Move(3,4));
+
+		 testPlayer1.forceMove(new Move(3,6));
+		 testPlayer2.opponentMove(new Move(3,6));
+
+		 testPlayer1.forceMove(new Move(5,1));
+		 testPlayer2.opponentMove(new Move(5,1));
+
+		 testPlayer1.forceMove(new Move(5,2));
+		 testPlayer2.opponentMove(new Move(5,2));
+
+		 testPlayer1.forceMove(new Move(5,4));
+		 testPlayer2.opponentMove(new Move(5,4));
+
+		 p(testPlayer1.getBoard());
+		 p(testPlayer2.getBoard());
+
+		 testPlayer2.forceMove(new Move(2,1));
+		 testPlayer1.opponentMove(new Move(2,1));
+
+		 testPlayer2.forceMove(new Move(2,2));
+		 testPlayer1.opponentMove(new Move(2,2));
+
+		 testPlayer2.forceMove(new Move(2,4));
+		 testPlayer1.opponentMove(new Move(2,4));
+
+		 testPlayer2.forceMove(new Move(2,5));
+		 testPlayer1.opponentMove(new Move(2,5));
+
+		 testPlayer2.forceMove(new Move(4,1));
+		 testPlayer1.opponentMove(new Move(4,1));
+
+		 testPlayer2.forceMove(new Move(4,2));
+		 testPlayer1.opponentMove(new Move(4,2));
+
+		 testPlayer2.forceMove(new Move(4,4));
+		 testPlayer1.opponentMove(new Move(4,4));
+
+		 testPlayer2.forceMove(new Move(4,6));
+		 testPlayer1.opponentMove(new Move(4,6));
+
+		 testPlayer2.forceMove(new Move(6,1));
+		 testPlayer1.opponentMove(new Move(6,1));
+
+		 testPlayer2.forceMove(new Move(6,2));
+		 testPlayer1.opponentMove(new Move(6,2));
+
+		 testPlayer2.forceMove(new Move(6,4));
+		 testPlayer1.opponentMove(new Move(6,4));
+
+		 p(testPlayer1.getBoard());
+		 p(testPlayer2.getBoard());
+		 while(!testPlayer1.getBoard().checkNetwork(Square.WHITE).hasNetwork() && !testPlayer2.getBoard().checkNetwork(Square.BLACK).hasNetwork()){
+		 	Move temp = testPlayer1.chooseMove();
+		 	p(temp);
+		 	p(testPlayer2.opponentMove(temp));
+		 	p(testPlayer1.getBoard());
+		 	temp = testPlayer2.chooseMove();
+		 	p(temp);
+		 	p(testPlayer1.opponentMove(temp));
+		 	p(testPlayer2.getBoard());
+		 }
+		 p("White has won: " + testPlayer1.getBoard().checkNetwork(Square.WHITE).hasNetwork());
+		 p("black has won: " + testPlayer1.getBoard().checkNetwork(Square.BLACK).hasNetwork());
+    }
+}
